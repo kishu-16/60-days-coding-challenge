3d64fdb3b4258b9fcef9752e15269586ee86944c
kishu-16 <168007472+kishu-16@users.noreply.github.com>
2026-02-01 21:35:26 +0530

    Add files via upload
    `Day 1 - Two Sum Solution`

---
+++ Day -1 Two Sum/twosum.py
@@ -0,0 +1,15 @@
+# Two Sum
+nums = [2, 7, 11, 15]
+target = 9
+
+def twoSum(nums, target):
+    hash_map = {}
+
+    for i, num in enumerate(nums):
+        if target - num in hash_map:
+            return [hash_map[target - num], i]
+        hash_map[num] = i
+
+    return []
+
+print(twoSum(nums, target))
